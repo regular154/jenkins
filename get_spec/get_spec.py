6e2dbf1c5ab0d62f5b4917a1f1c0691b9dedc7af
regular154 <48786363+regular154@users.noreply.github.com>
2022-10-07 20:31:10 +0300

    Update get_spec.py

--- get_spec/get_spec.py
+++ get_spec/get_spec.py
@@ -18,17 +18,31 @@ class SpecGenerator:
     def _configure_console_parser():
         parser = argparse.ArgumentParser()
         parser.add_argument('--url', type=str, help='Graphql server introspection url')
+        parser.add_argument('--title', type=str, help='Title')
+        parser.add_argument('--description', type=str, help='Description')
         return parser.parse_args()
 
-    def generate(self, url):
+    def get_spec(self, url):
         headers = {'Content-Type': 'application/json'}
         response = requests.post(url=url, data=introspection_query, headers=headers)
         spec_json = response.json()
         with open("spec.json", "w") as outf:
             outf.write(str(json.dumps(spec_json)))
+    
+    def modify_config(self, url):
+        with open("config.yml", "r") as stream:
+            try:
+                config = yaml.safe_load(stream)
+                config['info']['title'] = title
+                config['info']['description'] = description
+            except yaml.YAMLError as exc:
+                warnings.warn(f"FAIL: Config file couldn't be modified because - {exc}")
+        with open("config_modified.yml", 'w') as outf:
+            yaml.dump(config, outf, default_flow_style=False)
 
 
 if __name__ == '__main__':
     arguments = SpecGenerator._configure_console_parser() 
     publish_api_specs = SpecGenerator()
-    publish_api_specs.generate(arguments.url)
+    publish_api_specs.get_spec(arguments.url)
+    publish_api_specs.modify_config(arguments.title, arguments.description)
